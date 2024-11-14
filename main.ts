71ce3664a826d19f6c64cd8f268bc7c5ce526498
MrMaksik492 <134811622+mrmaksik492@users.noreply.github.com>
2024-11-14 19:19:29 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+function SetLed (Led: string, OnOff: number) {
+    radio.sendValue(Led, OnOff)
+    serial.writeValue(Led, OnOff)
+}
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
+        for (let index2 = 0; index2 <= 4; index2++) {
+            led.plot(index2, index)
+            while (!(input.buttonIsPressed(Button.A))) {
+                if (input.buttonIsPressed(Button.B)) {
+                    SetLed("" + index2 + index, 1)
+                }
+                if (input.logoIsPressed()) {
+                    SetLed("" + index2 + index, 0)
+                }
+                basic.pause(100)
+            }
+            basic.pause(100)
+            led.unplot(index2, index)
+        }
+    }
+})
